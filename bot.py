710cd0c9eff99e2100c8baf4de57f77b4a44d796
Ceko Hub <Khadishat10061993@icloud.com>
2026-08-18 01:10:49 +0300

    Update and rename index.py to bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ from telegram.ext import (
 # CEKO HUB — FULL BOT
 # ============================================================
 
-TOKEN = "ВСТАВЬ_НОВЫЙ_ТОКЕН_БОТА"
+TOKEN = "8905175157:AAEXGCH_Cx2On1uH0JMuBoEuxDB3I2F52N0"
 ADMIN_ID = 8161017993
 
 ADMIN_USERNAME = "@netuzu"
